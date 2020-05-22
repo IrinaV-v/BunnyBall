465611dbf5417c74a1e2d6a04000396dab97d48a
IrinaV-v <smirnovairina00@bk.ru>
2020-05-22 11:23:03 +0300

    All Works!

---
+++ Assets/Scripts/GameInstructions.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameInstructions : MonoBehaviour
+{
+    string gameInstructions = "To run the game use keys W, S, A, D";
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(gameInstructions);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
